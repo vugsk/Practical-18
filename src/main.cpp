14e45eb500cf95d652f241066632f00e329efcc6
vugsk <ganchukovnikita@bk.ru>
2024-03-03 19:24:42 +0700

    место традиционного указателя использовал умный указатель

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 
 #include <algorithm>
-#include <functional>
 #include <iostream>
 
 #include <Config.hpp>
+#include <memory>
 
 using namespace std;
 using placeholders::_1;
@@ -29,9 +29,10 @@ private:
     wstring _value;
 };
 
+
+
 static wstring input;
 static int     pos;
-
 
 wstring test_func_wstring(const wchar_t quote)
 {
@@ -40,35 +41,35 @@ wstring test_func_wstring(const wchar_t quote)
 }
 
 template<typename F>
-pair<bool, Token*> test_func_(const vector<pair<TokenType, wstring>>& vec,
+pair<bool, shared_ptr<Token>> test_func_(const vector<pair<TokenType, wstring>>& vec,
     const F& func)
 {
     for (const auto& [_token, ch] : vec)
         if (func(ch))
-            return {true, new Token(_token, ch)};
+            return {true, make_shared<Token>(_token, ch)};
     return {false, nullptr};
 }
 
-Token* identifierOrKeyword()
+shared_ptr<Token> identifierOrKeyword()
 {
     const wstring sb = test_st(input, pos, IsLetterOrDigit);
     if (test_func_(TYPE_DATA_, bind(ranges::equal, sb, _1)).first)
         return test_func_(TYPE_DATA_, bind(ranges::equal, sb, _1)).second;
-    return new Token(TokenType::ID, sb);
+    return make_shared<Token>(TokenType::ID, sb);
 }
 
-vector<Token*> test_func(const wstring& input_text)
+vector<shared_ptr<Token>> test_func(const wstring& input_text)
 {
     input = input_text;
-    vector<Token*> token;
+    vector<shared_ptr<Token>> token;
     for(pos = 0; pos < input.size(); pos++)
     {
         if (is_func(input[pos]))
-            token.push_back(new Token(TokenType::STRING_LITERAL,
+            token.push_back(make_shared<Token>(TokenType::STRING_LITERAL,
                 test_func_wstring(input[pos])));
 
         if (isdigit(input[pos]))
-            token.push_back(new Token(TokenType::NUMBER_LITERAL,
+            token.push_back(make_shared<Token>(TokenType::NUMBER_LITERAL,
                 test_st(input, pos, IsDigit)));
 
         if (IsSymbol(input[pos]))
@@ -78,7 +79,7 @@ vector<Token*> test_func(const wstring& input_text)
             token.push_back(test_func_(TYPE_CHAR_, bind(test_func_bool, input[pos], _1)).second);
     }
 
-    token.push_back(new Token(TokenType::END, END));
+    token.push_back(make_shared<Token>(TokenType::END, END));
     return token;
 }
 
@@ -93,13 +94,9 @@ int main()
 
     std::wcout << L"Код:\n" << filename << std::endl;
 
-    const std::vector<Token*> tokens = test_func(filename);
-    for (const auto i : tokens)
+    for (const auto& i : test_func(filename))
         std::wcout << static_cast<int>(i->getToken()) << ' '
                     << i->getValue() << std::endl;
-
-    for (const auto& i : tokens)
-        delete i;
 
     return 0;
 }
